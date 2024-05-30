23b4dd8fdf9e66ce4d7ce54d3cea038cf4abff40
Yous113 <yono@itu.dk>
2024-05-30 18:31:10 +0200

    First iteration shooting

---
+++ GayBuddies/Assets/Script/Player/PlayerShooting.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletSpeed = 15.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.velocity = bulletSpawn.up * bulletSpeed;
+        }
+    }
+}
