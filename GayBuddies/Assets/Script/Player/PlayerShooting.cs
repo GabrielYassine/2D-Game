8687890d2637953a7ce55843b7a1f3144f8931fe
Yous113 <yono@itu.dk>
2024-06-01 13:12:05 +0200

    Update PlayerShooting.cs

--- GayBuddies/Assets/Script/Player/PlayerShooting.cs
+++ GayBuddies/Assets/Script/Player/PlayerShooting.cs
@@ -7,12 +7,16 @@ public class PlayerShooting : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public float bulletSpeed = 15.0f;
+    public float fireRate = 1.0f; // Number of times player can shoot per second
+
+    private float nextFire = 0.0f; // Time when player can shoot again
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
+            nextFire = Time.time + 1.0f / fireRate; // Calculate the next time the player can fire
             GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             rb.velocity = bulletSpawn.up * bulletSpeed;
