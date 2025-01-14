74bc9f23acc74b33befffb372b9a70f6dd7e2134
s235257 <Gabi04@live.dk>
2025-01-14 20:09:53 +0100

    Multiplayer scripts update on each screen now

--- GayBuddies/Assets/Script/Player/HealthScript.cs
+++ GayBuddies/Assets/Script/Player/HealthScript.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using FishNet.Object;
+using FishNet.Connection;
 using UnityEngine;
 
-public class HealthScript : MonoBehaviour
+public class HealthScript : NetworkBehaviour
 {
     public float health = 100.0f;
 
     public void TakeDamage(float damage)
     {
-        health -= damage;
-        if (health <= 0.0f)
+        if (IsServer)
         {
-            Destroy(gameObject);
+            health -= damage;
+            if (health <= 0.0f)
+            {
+                Die();
+            }
+            UpdateHealth(health);
         }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player has died.");
+        Destroy(gameObject);
+    }
+
+    [ObserversRpc] // This method will be called on all clients
+    private void UpdateHealth(float newHealth)
+    {
+        health = newHealth;
+        Debug.Log("Updated health: " + health);
     }
 }
--- GayBuddies/Assets/Script/Player/PlayerMovement.cs
+++ GayBuddies/Assets/Script/Player/PlayerMovement.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FishNet.Object;
+using FishNet.Connection;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : NetworkBehaviour {
 
     public float runSpeed = 40f;
     float horizontalMove = 0f;
@@ -11,14 +13,10 @@ public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D controller;
     public Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update() {
+        if (!IsOwner) return; // Only allow the local player to control their character
+
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
         if (Input.GetButtonDown("Jump")) {
@@ -39,7 +37,24 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        if (!IsOwner) return; // Only allow the local player to control their character
+
+        Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+    }
+
+    [ServerRpc]
+    private void Move(float move, bool crouch, bool jump) {
+        controller.Move(move, crouch, jump);
+        UpdateClientMove(move, crouch, jump);
+    }
+
+    [ObserversRpc]
+    private void UpdateClientMove(float move, bool crouch, bool jump) {
+        if (IsOwner) { // Skip updating the owner's client (no need to update the owner's client since they are the one controlling the character)
+            return;
+        }
+
+        controller.Move(move, crouch, jump);
     }
 }
